2efc9c39a3fe357f7f84cd582d9cf40316582c2f
Татьяна <ltasya72@gmail.com>
2023-10-26 17:16:06 +0300

    Закончена вёрстка category

--- app/js/main.js
+++ app/js/main.js
@@ -4,17 +4,17 @@
 //   menu.classList.toggle("header__menu-list--active");
 // });
 
-$(function () {
-  $(".hero__slider").slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow:
-      '<button type="button" class="hero__slider-arrow hero__slider-arrow-next"><svg class="hero__slider-svg" width="15" height="28" aria-label="next arrow"><use href="./images/icons.svg#next"></use></svg></button> ',
-    prevArrow:
-      '<button type="button" class="hero__slider-arrow hero__slider-arrow-prev"><svg class="hero__slider-svg" width="15" height="28" aria-label="prev arrow"><use href="./images/icons.svg#prev"></use></svg></button> ',
-  });
-});
+// $(function () {
+//   $(".hero__slider").slick({
+//     dots: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     nextArrow:
+//       '<button type="button" class="hero__slider-arrow hero__slider-arrow-next"><svg class="hero__slider-svg" width="15" height="28" aria-label="next arrow"><use href="./images/icons.svg#next"></use></svg></button> ',
+//     prevArrow:
+//       '<button type="button" class="hero__slider-arrow hero__slider-arrow-prev"><svg class="hero__slider-svg" width="15" height="28" aria-label="prev arrow"><use href="./images/icons.svg#prev"></use></svg></button> ',
+//   });
+// });
 // ! Tabs
 $(".search__tabs-link").on('click', function (e) {
   e.preventDefault();
