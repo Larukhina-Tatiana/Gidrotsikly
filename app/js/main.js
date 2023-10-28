161da97d5ca17455655eacbb3671fe0833fe68ad
Татьяна <ltasya72@gmail.com>
2023-10-28 11:35:15 +0300

    products - slider

--- app/js/main.js
+++ app/js/main.js
@@ -16,14 +16,31 @@
 //   });
 // });
 // ! Tabs
-$(".search__tabs-link").on('click', function (e) {
+$(".tabs__link").on('click', function (e) {
   e.preventDefault();
 
-  $(".search__tabs-link").removeClass("search__tabs-link--active");
-  $(".search__content-item").removeClass("search__content-item--active");
+  $(".tabs__link").removeClass("tabs__link--active");
+  $(".tabs__content").removeClass("tabs__content--active");
 
-  $(this).addClass("search__tabs-link--active");
-  $($(this).attr("href")).addClass("search__content-item--active");
+  $(this).addClass("tabs__link--active");
+  $($(this).attr("href")).addClass("tabs__content--active");
+});
+
+$(".products__card-favorite").on("click", function () {
+  $(".products__card-favorite").toggleClass("products__card-favorite--active");
+});
+
+
+$(function () {
+  $(".products__slider").slick({
+//     dots: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+    nextArrow:
+      '<button type="button" class="hero__slider-arrow hero__slider-arrow-next"><svg class="hero__slider-svg" width="15" height="28" aria-label="next arrow"><use href="./images/icons.svg#next"></use></svg></button> ',
+    prevArrow:
+      '<button type="button" class="hero__slider-arrow hero__slider-arrow-prev"><svg class="hero__slider-svg" width="15" height="28" aria-label="prev arrow"><use href="./images/icons.svg#prev"></use></svg></button> ',
+  });
 });
 
 // var mixer = mixitup(".portfolio__box", {
