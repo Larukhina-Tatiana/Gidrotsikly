1316e43ab6a02994ebcbb00c74ada6bd5f3dba6a
Татьяна <ltasya72@gmail.com>
2023-10-30 17:34:11 +0200

    свёрстан footer

--- app/js/main.js
+++ app/js/main.js
@@ -15,16 +15,19 @@ $(function () {
       '<button type="button" class="hero__slider-arrow hero__slider-arrow-prev"><svg class="hero__slider-svg" width="15" height="28" aria-label="prev arrow"><use href="./images/icons.svg#prev"></use></svg></button> ',
   });
 });
+
 // ! Tabs
 $(".tabs__link").on('click', function (e) {
   e.preventDefault();
 
-  $(".tabs__link").removeClass("tabs__link--active");
-  $(".tabs__content").removeClass("tabs__content--active");
+  $($(this).siblings()).removeClass("tabs__link--active");
+  $($(this).parents().siblings().find("tabs__content")).removeClass("tabs__content--active");
 
   $(this).addClass("tabs__link--active");
   $($(this).attr("href")).addClass("tabs__content--active");
 });
+
+
 
 // Иконка фаворит становиться залитой
 
